f34c5ce4a13546259c9b65dec1501d2d7e5775a5
vixcera <vixceraa@gmail.com>
2024-01-08 23:10:15 +0700

    bug fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import compression from "compression";
 import cookieParser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
-import limiter from "express-rate-limit"
 import express from 'express';
 import helmet from "helmet";
 import dotenv from 'dotenv';
@@ -16,7 +15,6 @@ connectDB()
 app.use("trust proxy", true)
 app.use(cors({ credentials: true, origin: ['https://vixcera.my.id', 'http://localhost:5173'] }));
 app.use(helmet.contentSecurityPolicy({ directives: { "script-src" : ["'self'", "code.jquery.com", "cdn.jsdelivr.net"] } }))
-app.use(limiter({ windowMs: 1 * 60 * 1000 , max: 20 }))
 app.use(express.urlencoded({ extended: true }));
 app.use(compression())
 app.use(fileUpload());
