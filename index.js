d71c39517091bb1b9e02235f2792a15fa4dd2be5
vixcera <vixceraa@gmail.com>
2024-01-08 22:22:55 +0700

    add file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import fileUpload from 'express-fileupload';
+import compression from "compression";
 import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
+import limiter from "express-rate-limit"
 import express from 'express';
+import helmet from "helmet";
 import dotenv from 'dotenv';
 import http from 'http';
 import cors from 'cors';
@@ -12,8 +15,12 @@ const port = 3000;
 const server = new http.createServer(app);
 
 dotenv.config();
+
 app.use(cors({ credentials: true, origin: ['https://vixcera.my.id', 'http://localhost:5173'] }));
+app.use(helmet.contentSecurityPolicy({ directives: { "script-src" : ["'self'", "code.jquery.com", "cdn.jsdelivr.net"] } }))
+app.use(limiter({ windowMs: 1 * 60 * 1000 , max: 20 }))
 app.use(express.urlencoded({ extended: true }));
+app.use(compression())
 app.use(fileUpload());
 app.use(express.json());
 app.use(cookieParser());
