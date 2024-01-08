96612890e0890c63d152fe3fed1d65d63652f448
vixcera <vixceraa@gmail.com>
2024-01-08 23:12:19 +0700

    bug fix

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const app = express();
 dotenv.config();
 connectDB()
 
-app.use("trust proxy", true)
 app.use(cors({ credentials: true, origin: ['https://vixcera.my.id', 'http://localhost:5173'] }));
 app.use(helmet.contentSecurityPolicy({ directives: { "script-src" : ["'self'", "code.jquery.com", "cdn.jsdelivr.net"] } }))
 app.use(express.urlencoded({ extended: true }));
