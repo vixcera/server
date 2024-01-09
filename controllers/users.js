14f94a09888329d8341efcdfd071f92137a1ec46
vixcera <vixceraa@gmail.com>
2024-01-09 12:17:13 +0700

    bug fix

--- controllers/users.js
+++ controllers/users.js
@@ -40,7 +40,7 @@ export const user_login = async (request, response) => {
     const now = date({ date: new Date(), showMilliseconds: true });
 
     response.cookie('reftoken', reftoken, {
-      httpOnly: true, sameSite: 'None', secure: true, domain: 'vixcera.my.id', maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true, domain: 'vixcera.my.id', sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000,
     });
     response.json({ token });
   } catch (error) {
